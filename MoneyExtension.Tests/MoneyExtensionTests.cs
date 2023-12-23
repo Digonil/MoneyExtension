60f7ff8c4a45fd75dc6a559260c2f1e33ef67c6f
Diego Niles <diego.niles@hotmail.com>
2023-12-23 12:26:46 -0300

    Criado a classe de conversão e sua classe de teste

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,16 @@
+namespace MoneyExtension.Tests;
+
+[TestClass]
+public class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertToInt()
+    {
+        decimal valor = 279.98M;
+
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
+
+    }
+}
